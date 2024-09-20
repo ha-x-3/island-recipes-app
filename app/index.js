3325a4a36fcbbdf58d98dd72b92f63ef302af8fe
Brittney Epperson <beppers87@hotmail.com>
2024-09-20 18:23:33 -0500

    Updated icons and added image

--- app/index.js
+++ app/index.js
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View, Platform } from 'react-native';
 
 export default function App() {
+
+    const hero = require('../assets/islandRecipes.png');
+
 	return (
 		<View style={styles.container}>
+            <Image source={hero} style={styles.heroStyle}></Image>
 			<Text>Expo Router is working because this is the new homepage!</Text>
 			<StatusBar style='auto' />
 		</View>
@@ -13,8 +17,24 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'rgba(2, 169, 157, 1.0)',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	heroStyle: {
+		height: 120,
+		width: '100%',
+		alignSelf: 'center',
+		...Platform.select({
+			android: {
+				top: '-10%',
+			},
+			ios: {
+				top: '-10%',
+			},
+			default: {
+				top: '-3%',
+			},
+		}),
 	},
 });
