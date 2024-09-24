4f403e43999ac9e28391f0f3a500f4983b9feb29
Brittney Epperson <beppers87@hotmail.com>
2024-09-24 13:38:14 -0500

    Refactored custom font loading from index to app.js

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded, fontError] = useFonts({
+		OpenSans: require('../assets/fonts/OpenSans-Regular.ttf'),
+		'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
+		'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+		'OpenSans-SemiBold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
+		'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync();
+		}
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+		return null;
+  }
+
+	return (
+		<View
+			style={styles.container}
+			onLayout={onLayoutRootView}
+		>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
--- app/index.js
+++ app/index.js
@@ -1,38 +1,13 @@
-import { useCallback } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View, Platform } from 'react-native';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
-
-export default function App() {
-
-    const [fontsLoaded, fontError] = useFonts({
-		'OpenSans': require('../assets/fonts/OpenSans-Regular.ttf'),
-        'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
-        'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-        'OpenSans-SemiBold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
-        'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf')
-	});
-
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded || fontError) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded, fontError]);
-
-    if (!fontsLoaded && !fontError) {
-        return null;
-    }
+export default function Home() {
 
     const hero = require('../assets/islandRecipes.png');
 
 	return (
-		<View style={styles.container} onLayout={onLayoutRootView}>
+		<View style={styles.container}>
             <Image source={hero} style={styles.heroStyle}></Image>
 			<Text style={{ fontFamily: 'OpenSans-BoldItalic', fontSize: 20 }}>Custom font is working because this is the new homepage!</Text>
-			<StatusBar style='auto' />
 		</View>
 	);
 }
