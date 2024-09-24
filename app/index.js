c0f7ab1bc65e883e25b77622caa5d9d0054301cb
Brittney Epperson <beppers87@hotmail.com>
2024-09-24 13:22:51 -0500

    Added font/splash screen loading for custom fonts

--- app/index.js
+++ app/index.js
@@ -1,14 +1,37 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Image, Text, View, Platform } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+    const [fontsLoaded, fontError] = useFonts({
+		'OpenSans': require('../assets/fonts/OpenSans-Regular.ttf'),
+        'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
+        'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+        'OpenSans-SemiBold': require('../assets/fonts/OpenSans-SemiBold.ttf'),
+        'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf')
+	});
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
 
     const hero = require('../assets/islandRecipes.png');
 
 	return (
-		<View style={styles.container}>
+		<View style={styles.container} onLayout={onLayoutRootView}>
             <Image source={hero} style={styles.heroStyle}></Image>
-			<Text>Expo Router is working because this is the new homepage!</Text>
+			<Text style={{ fontFamily: 'OpenSans-BoldItalic', fontSize: 20 }}>Custom font is working because this is the new homepage!</Text>
 			<StatusBar style='auto' />
 		</View>
 	);
@@ -19,21 +42,23 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: 'rgba(2, 169, 157, 1.0)',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'flex-start',
 	},
 	heroStyle: {
-		height: 120,
+		height: 135,
 		width: '100%',
 		alignSelf: 'center',
+        resizeMode: 'contain',
+        marginBottom: 20,
 		...Platform.select({
 			android: {
-				top: '-10%',
+				top: 5,
 			},
 			ios: {
-				top: '-10%',
+				top: 5,
 			},
 			default: {
-				top: '-3%',
+				top: 5,
 			},
 		}),
 	},
