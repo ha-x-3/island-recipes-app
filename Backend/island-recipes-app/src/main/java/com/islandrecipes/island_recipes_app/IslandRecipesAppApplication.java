873d25beda2ced01d430101b2b1d02fd7c84bb4a
Brittney Epperson <beppers87@hotmail.com>
2024-10-02 12:00:53 -0500

    Added Backend build initial files

---
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/IslandRecipesAppApplication.java
@@ -0,0 +1,13 @@
+package com.islandrecipes.island_recipes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandRecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandRecipesAppApplication.class, args);
+	}
+
+}
---
+++ Backend/island-recipes-app/src/test/java/com/islandrecipes/island_recipes_app/IslandRecipesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.islandrecipes.island_recipes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandRecipesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
