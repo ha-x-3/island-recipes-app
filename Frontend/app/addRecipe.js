ba3c55f4ca3fe4775b607a088ff62c6a360678a3
Brittney Epperson <beppers87@hotmail.com>
2024-10-07 13:20:20 -0500

    Started add recipe page

--- Frontend/app/addRecipe.js
+++ Frontend/app/addRecipe.js
@@ -1,10 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
+import { Formik, FieldArray } from 'formik';
+import * as Yup from 'yup';
 
 export default function addRecipe() {
 	return (
-		<View>
-			<Text>addRecipe</Text>
+		<View style={styles.container}>
+			<Formik
+				initialValues={{
+					recipeName: '',
+					yield: '',
+					prepTime: '',
+					cookTime: '',
+					ingredients: [{ name: '', amount: '', unit: '' }],
+					instructions: '',
+					recipePhoto: '',
+				}}
+			>
+				{({
+					values,
+					handleChange,
+					handleSubmit,
+					setFieldValue,
+					isSubmitting,
+					errors,
+					touched,
+				}) => 
+				<View>
+					{/* Recipe Name */}
+					<Text>Recipe Name</Text>
+					<TextInput
+						onChangeText={handleChange('recipeName')}
+						value={values.recipeName}
+						placeholder='Recipe Name'
+					></TextInput>
+					{touched.recipeName && errors.recipeName && <Text>{errors.recipeName}</Text>}
+				</View>}
+			</Formik>
 		</View>
 	);
 }
