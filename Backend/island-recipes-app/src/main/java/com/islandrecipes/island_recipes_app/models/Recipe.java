e7211ebeba18c875120253c89849c51402393e31
Brittney Epperson <beppers87@hotmail.com>
2024-10-14 16:31:26 -0500

    Added Recipe model

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/Recipe.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/Recipe.java
@@ -1,4 +1,36 @@
 package com.islandrecipes.island_recipes_app.models;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Document(collection = "recipes")
 public class Recipe {
+
+    @Id
+    private String id;
+    private String recipeName;
+    private int yield;
+    private int prepTimeHour;
+    private int prepTimeMin;
+    private int cookTimeHour;
+    private int cookTimeMin;
+    private List<Ingredient> ingredients;
+    private String instructions;
+    private String recipePhotoUrl;
+
+    @Setter
+    @Getter
+    public static class Ingredient {
+        private String name;
+        private double amount;
+        private String unit;
+
+    }
+
 }
