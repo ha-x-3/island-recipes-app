9d75a36aeccf5d5cae94bcf007e8a48034a3cc53
Brittney Epperson <beppers87@hotmail.com>
2024-10-15 10:27:59 -0500

    Added recipe controller

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/controllers/RecipeController.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/controllers/RecipeController.java
@@ -1,4 +1,30 @@
 package com.islandrecipes.island_recipes_app.controllers;
 
+import com.islandrecipes.island_recipes_app.models.Recipe;
+import com.islandrecipes.island_recipes_app.services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recipes")
 public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @Autowired
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @PatchMapping
+    public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
+        Recipe savedRecipe = recipeService.saveRecipe(recipe);
+        return new ResponseEntity<>(savedRecipe, HttpStatus.CREATED);
+    }
+
 }
