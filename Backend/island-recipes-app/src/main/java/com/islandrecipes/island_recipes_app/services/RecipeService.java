707f9f0d74bd1d852602930fe0ed2a8bf935de96
Brittney Epperson <beppers87@hotmail.com>
2024-10-15 10:24:45 -0500

    Added recipe service

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/services/RecipeService.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/services/RecipeService.java
@@ -1,4 +1,22 @@
 package com.islandrecipes.island_recipes_app.services;
 
+import com.islandrecipes.island_recipes_app.models.Recipe;
+import com.islandrecipes.island_recipes_app.repositories.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Recipe saveRecipe(Recipe recipe) {
+        return recipeRepository.save(recipe);
+    }
+
 }
