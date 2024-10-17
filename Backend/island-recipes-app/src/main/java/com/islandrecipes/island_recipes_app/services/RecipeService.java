3be2ec8e22447882323e993c90c8b16daac82ae0
Brittney Epperson <beppers87@hotmail.com>
2024-10-17 15:11:07 -0500

    Added getRecipeById to Service

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/services/RecipeService.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/services/RecipeService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -23,6 +24,10 @@ public class RecipeService {
 
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
+    }
+
+    public Optional<Recipe> getRecipeById(String id) {
+        return recipeRepository.findById(id);
     }
 
 }
