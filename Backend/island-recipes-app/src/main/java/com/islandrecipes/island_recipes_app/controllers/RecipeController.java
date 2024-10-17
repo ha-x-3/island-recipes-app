dba3fee1d8d12117bd59f02a4df81edfee28de2f
Brittney Epperson <beppers87@hotmail.com>
2024-10-17 12:56:47 -0500

    Added GET mapping to retrieve all recipes to controller

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/controllers/RecipeController.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/controllers/RecipeController.java
@@ -5,10 +5,9 @@ import com.islandrecipes.island_recipes_app.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -25,6 +24,12 @@ public class RecipeController {
     public ResponseEntity<Recipe> createRecipe(@RequestBody Recipe recipe) {
         Recipe savedRecipe = recipeService.saveRecipe(recipe);
         return new ResponseEntity<>(savedRecipe, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Recipe>> getAllRecipes() {
+        List<Recipe> recipes = recipeService.getAllRecipes();
+        return new ResponseEntity<>(recipes, HttpStatus.OK);
     }
 
 }
