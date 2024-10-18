5137e247eee00463228b8b592c41b9192192fe01
Brittney Epperson <beppers87@hotmail.com>
2024-10-18 10:21:57 -0500

    Added query function to repository

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/repositories/RecipeRepository.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/repositories/RecipeRepository.java
@@ -2,6 +2,14 @@ package com.islandrecipes.island_recipes_app.repositories;
 
 import com.islandrecipes.island_recipes_app.models.Recipe;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface RecipeRepository extends MongoRepository<Recipe, String> {
+
+    // Custom query to search by recipe name and ingredients name using regex
+    @Query("{'$or': [{'recipeName': {$regex: ?0, $options: 'i'}}, {'ingredients.name': {$regex: ?0, $options: 'i'}}]}")
+    List<Recipe> searchRecipesByNameOrIngredient(String searchTerm);
+
 }
