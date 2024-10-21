984157b158e80e9b2cf20f2ee90f20cecc6b7895
Brittney Epperson <beppers87@hotmail.com>
2024-10-21 14:36:10 -0500

    Added Jackson JSON annotations for total nutrients

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/NutritionalData.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/NutritionalData.java
@@ -2,27 +2,33 @@ package com.islandrecipes.island_recipes_app.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 @Setter
 public class NutritionalData {
-
     private double calories;
     private TotalNutrients totalNutrients;
 
     @Getter
     @Setter
     public static class TotalNutrients {
-        private Nutrient FAT;
-        private Nutrient PROCNT;
-        private Nutrient CHOLE;
-        private Nutrient NA;
+        @JsonProperty("FAT")
+        private NutrientInfo fat;
+        @JsonProperty("PROCNT")
+        private NutrientInfo protein;
+        @JsonProperty("CHOLE")
+        private NutrientInfo cholesterol;
+        @JsonProperty("NA")
+        private NutrientInfo sodium;
     }
 
     @Getter
     @Setter
-    public static class Nutrient {
+    public static class NutrientInfo {
+        private String label;
         private double quantity;
         private String unit;
     }
+    
 }
