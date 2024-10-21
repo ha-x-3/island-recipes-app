a0e63a62c93d9196e6121bb7a52aefaa3dac90ea
Brittney Epperson <beppers87@hotmail.com>
2024-10-21 13:37:35 -0500

    Added chosen nutrients to nutritional data

--- Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/NutritionalData.java
+++ Backend/island-recipes-app/src/main/java/com/islandrecipes/island_recipes_app/models/NutritionalData.java
@@ -1,4 +1,28 @@
 package com.islandrecipes.island_recipes_app.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NutritionalData {
+
+    private double calories;
+    private TotalNutrients totalNutrients;
+
+    @Getter
+    @Setter
+    public static class TotalNutrients {
+        private Nutrient FAT;
+        private Nutrient PROCNT;
+        private Nutrient CHOLE;
+        private Nutrient NA;
+    }
+
+    @Getter
+    @Setter
+    public static class Nutrient {
+        private double quantity;
+        private String unit;
+    }
 }
